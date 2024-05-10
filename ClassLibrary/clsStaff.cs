4b895b82efb82eb173c0f8a7ab840ec91c538764
P2723373 <P2723373@my365.dmu.ac.uk>
2024-05-10 17:23:08 +0100

    I have created properties for staff table practical 7 work

--- ClassLibrary/clsStaff.cs
+++ ClassLibrary/clsStaff.cs
@@ -1,6 +1,15 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsStaff
     {
+        public bool Active { get; set; }
+        public int StaffId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime HireDate { get; set; }
+        public decimal Salary { get; set; }
     }
 }
