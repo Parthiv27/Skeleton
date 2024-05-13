c9457952c9da322028decd93802bef632893e077
p2722712 <p2722712@GWHXX281WIN015W>
2024-05-13 14:40:17 +0100

    added 8 tests in stock, all passed.

--- Testing3/tstStock.cs
+++ Testing3/tstStock.cs
@@ -13,5 +13,99 @@ namespace Testing3
 
             Assert.IsNotNull(AnStock);
         }
+
+        [TestMethod]
+        public void StockIDPropertyOK()
+        {
+            clsStock AnStock = new clsStock();
+
+            int TestData = 1;
+
+            AnStock.StockId = TestData;
+
+            Assert.AreEqual(AnStock.StockId, TestData);
+        }
+
+
+        [TestMethod]
+        public void ProducttypePropertyOK()
+        {
+            clsStock AnStock = new clsStock();
+
+            string TestData = "Producttype";
+
+            AnStock.Producttype = TestData;
+
+            Assert.AreEqual(AnStock.Producttype, TestData);
+        }
+
+        [TestMethod]
+        public void SizePropertyOK()
+        {
+            clsStock AnStock = new clsStock();
+
+            string TestData = "Size";
+
+            AnStock.Size = TestData;
+
+            Assert.AreEqual(AnStock.Size, TestData);
+
+        }
+
+        [TestMethod]
+        public void StockQuantityPropertyOK()
+        {
+            clsStock AnStock = new clsStock();
+
+            int TestData = 4;
+
+            AnStock.StockQuantity = TestData;
+
+            Assert.AreEqual(AnStock.StockQuantity, TestData);
+
+        }
+
+
+        [TestMethod]
+        public void RestockneededPropertyOK()
+        {
+            clsStock AnStock = new clsStock();
+            
+            Boolean TestData = true;
+
+            AnStock.Restockneeded = TestData;
+
+            Assert.AreEqual(AnStock.Restockneeded, TestData);
+
+        }
+
+        [TestMethod]
+        public void DaterestockedPropertyOK()
+        {
+            clsStock AnStock = new clsStock();
+
+            DateTime TestData = DateTime.Now.Date;
+
+            AnStock.Daterestocked = TestData;
+
+            Assert.AreEqual(AnStock.Daterestocked, TestData);
+
+        }
+
+
+        [TestMethod]
+        public void DiscontinuedPropertyOK()
+        {
+            clsStock AnStock = new clsStock();
+
+            Boolean TestData = false;
+
+            AnStock.Discontinued = TestData;
+
+            Assert.AreEqual(AnStock.Discontinued, TestData);
+
+        }
+
     }
+
 }
