42539d47415d239572a613373e53b0a24c5597bd
btamoor1 <p2598304@my365.dmu.ac.uk>
2024-05-15 03:03:49 +0100

    added test classes

--- Testing2/UnitTest1.cs
+++ Testing2/UnitTest1.cs
@@ -14,5 +14,69 @@ namespace Testing2
             clsCustomer ACustomer = new clsCustomer();
             Assert.IsNotNull(ACustomer);
         }
+        [TestMethod]
+        public void CustomerIdPropertyOK()
+        {
+
+            clsCustomer ACustomer = new clsCustomer();
+            Int32 TestData = 1;
+            ACustomer.CustomerId = TestData;
+            Assert.AreEqual(AnCustomer.CustomerId, TestData);
+        }
+        [TestMethod]
+        public void FirstNamePropertyOK()
+        {
+
+            clsCustomer ACustomer = new clsCustomer();
+            string TestData = "FirstName";
+            ACustomer.FirstName = TestData;
+            Assert.AreEqual(ACustomer.FirstName, TestData);
+        }
+        [TestMethod]
+        public void SurNamePropertyOK()
+        {
+
+            clsCustomer ACustomer = new clsCustomer();
+            string TestData = "SurName";
+            ACustomer.SurName = TestData;
+            Assert.AreEqual(ACustomer.SurName, TestData);
+        }
+
+        [TestMethod]
+        public void EmailPropertyOK()
+        {
+
+            clsCustomer ACustomer = new clsCustomer();
+            string TestData = "Email";
+            ACustomer.Email = TestData;
+            Assert.AreEqual(ACustomer.Email, TestData);
+        }
+        [TestMethod]
+        public void AddressPropertyOK()
+        {
+
+            clsCustomer ACustomer = new clsCustomer();
+            string TestData = "Address";
+            ACustomer.Address = TestData;
+            Assert.AreEqual(ACustomer.Address, TestData);
+        }
+        [TestMethod]
+        public void DateAddedPropertyOK()
+        {
+            clsCustomer Acustomer = new clsCustomer();
+            DateTime TestData = DateTime.Date;
+            Acustomer.DateAdded = TestData; 
+            Assert.AreEqual(Acustomer.DateAdded, TestData);
+        }
+        [TestMethod]
+        public void ActiveCustomerPropertyOK()
+        {
+
+            clsCustomer ACustomer = new clsCustomer();
+            Boolean TestData = true;
+            ACustomer.Active = TestData;
+            Assert.AreEqual(ACustomer.Active, TestData);
+        }
     }
-}
+    }
+
