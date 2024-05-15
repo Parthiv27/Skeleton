7dbc112e21975c1d215fa6732ee8f5bad8eb7bd5
P2723373 <P2723373@my365.dmu.ac.uk>
2024-05-15 12:18:47 +0100

    Practical 8 done

--- AdminSystem/StaffDataEntry.aspx.cs
+++ AdminSystem/StaffDataEntry.aspx.cs
@@ -17,8 +17,20 @@ public partial class _1_DataEntry : System.Web.UI.Page
     {
         //create a new instance of clsStaff
         clsStaff AnStaff = new clsStaff();
+        //capture the StaffId
+        AnStaff.StaffId = Convert.ToInt32(txtStaffId.Text);
         //capture the firstname
-        AnStaff.FirstName = txtFirstName.Text; 
+        AnStaff.FirstName = txtFirstName.Text;
+        //capture the Lastname
+        AnStaff.LastName = txtLastName.Text;
+        //capture the Email
+        AnStaff.Email = txtEmail.Text;
+        //capture the HireDate
+        AnStaff.HireDate = Convert.ToDateTime(DateTime.Now);
+        //capture the Active Check box
+        AnStaff.Active = chkActive.Checked;
+        // Capture the Salary and convert it from string to decimal
+        AnStaff.Salary = Convert.ToDecimal(txtSalary.Text);
         //store the firstname in the session object
         Session["AnStaff"] = AnStaff;
         //navigate  to the  view page 
@@ -26,7 +38,4 @@ public partial class _1_DataEntry : System.Web.UI.Page
     }
 }
 
-internal class clsStaff
-{
-    public string FirstName { get; internal set; }
-}
+ 
--- AdminSystem/StaffViewer.aspx.cs
+++ AdminSystem/StaffViewer.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClassLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +10,17 @@ public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        //create a new instance of clsStaff
+        clsStaff AnStaff = new clsStaff();
+        //get the data from the session obejct 
+        AnStaff = (clsStaff)Session["AnStaff"];
+        //display the first name for this entry 
+        Response.Write(AnStaff.StaffId);
+        Response.Write(AnStaff.FirstName);
+        Response.Write(AnStaff.LastName);
+        Response.Write(AnStaff.Email);
+        Response.Write(AnStaff.HireDate);
+        Response.Write(AnStaff.Active);
+        Response.Write(AnStaff.Salary);
     }
 }
