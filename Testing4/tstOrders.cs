499dbb7ad5738af797437461bd919a96c4a95aff
Donald Jinadu <P2723245@my365.dmu.ac.uk>
2024-05-16 18:23:42 +0100

    practical 9a finished

--- Testing4/tstOrders.cs
+++ Testing4/tstOrders.cs
@@ -1,12 +1,16 @@
 ﻿using ClassLibrary;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Testing4
 {
     [TestClass]
     public class tstOrders
     {
+        public object AnCustomerName { get; private set; }
+        public object AnStockItem { get; private set; }
+
         [TestMethod]
         public void instanceOK()
         {
@@ -15,85 +19,96 @@ namespace Testing4
         }
         [TestMethod]
 
-        public void ActivePropertyOK()
+        public void DispatchedPropertyOK()
         {
             clsOrders AnOrders = new clsOrders();
 
             Boolean TestData = true;
 
-            AnOrders.Active = TestData;
+            AnOrders.Dispatched = TestData;
 
-            Assert.AreEqual(AnOrders.Active, TestData);
+            Assert.AreEqual(AnOrders.Dispatched, TestData);
         }
         public void DateAddedPropertyOK()
         {
             clsOrders AnDispatchDate = new clsOrders();
             DateTime TestData = DateTime.Now.Date;
-            AnDispatchDate.DateAdded = TestData;
-            Assert.AreEqual(AnDispatchDate.DateAdded, TestData);
+            AnDispatchDate.DispatchDate = TestData;
+            Assert.AreEqual(AnDispatchDate.DispatchDate, TestData);
         }
         [TestMethod]
-        public void FirstNameOK()
+        public void CustomerNameOK()
         {
-            clsOrders AnFirstName = new clsOrders();
-            string TestData = "Donald";
-            AnFirstName.FirstName = TestData;     
+            clsOrders AnCustomerName = new clsOrders();
+            string TestDate = "Donald Jinadu";
+            AnCustomerName.CustomerName = TestDate;
         }
         [TestMethod]
-        public void PostCodeOK()
+        public void StockItemOK()
         {
-            clsOrders AnPostCode = new clsOrders();
-            string TestData = "DA12 5UB";
-            AnPostCode.PostCode = TestData;
-            Assert.AreEqual(AnPostCode.PostCode,  TestData);
-        }
-        [TestMethod]
-        public void SurnameOK()
-        {
-            clsOrders AnSurname = new clsOrders();
-            string TestDate = "Jinadu";
-            AnSurname.Surname = TestDate;
-        }
-        [TestMethod]
-        public void ShoeOK()
-        {
-            clsOrders AnShoe = new clsOrders();
+            clsOrders AnStockItem = new clsOrders();
             string TestData = "Jordan 4s";
-            AnShoe.Shoe = TestData;
+            AnStockItem.StockItem = TestData;
         }
+
+        [TestMethod]
+        public void DescriptionOK()
+        {
+            clsOrders AnDescription = new clsOrders();
+            string TestData = " this shoe is a size 4 mens";
+            AnDescription.Description = TestData;
+        }
+        [TestMethod]
+        public void PriceOK()
+        {
+            //create an instance of the class we want to create 
+            clsOrders AnPrice = new clsOrders();
+            //create sp,e test data to assign to the property
+            Double testData = 100.00;
+            //assign the data to the property
+            AnPrice.Price = testData;
+            //test to see that the 2 values are the same
+            Assert.AreEqual(testData, AnPrice.Price);
+        }
+
+
+        
+
+
         [TestMethod]
         public void FindMethodOK()
         {         //create an instance of the class I want to create
             clsOrders AnOrders = new clsOrders();
             //create a boolean variable to store the results of the validation
-            Boolean found = false;
+            Boolean Found = false;
             //create some test data to use with the method 
             Int32 OrderID = 40;
             //invoke the method 
             Found = AnOrders.Find(OrderID);
             //test to see if the result is true
-            Assert.IsTrue(found);
+            Assert.IsTrue(Found);
         }
         [TestMethod]
-        public void testOrderIdFound()
+        public void testDispatchedFound()
         {
             //create an instance of the class we want to create
-            clsOrders AnOrder = new clsOrders();
-            //create a boolean variable to record if the data is OK (assumer it is)
-            boolean OK = true;
+            clsOrders AnDispatchDate = new clsOrders();
+            //create a boolean variable to store the result of the search
+            Boolean Found = false;
+            //create a boolean variable to record if the data is OK (assume it is)
+            Boolean OK = true;
             //create some test data to use with the method
-            int32 OrderID = 21;
+            Int32 OrderID = 21;
             //invoke the method
-            Found = AnOrder.Find(OrderID);
-            //chech the Order ID
-            if (AnOrder.OrderID == 21)
+            Found = AnDispatchDate.Find(OrderID);
+            //check the dispatch date property
+            if (AnDispatchDate.DispatchDate != Convert.ToDateTime("02/06/2004"))
+                //always return true
             {
                 OK = false;
             }
-            //test to see that the result is correct
-            Assert.isTrue(OK);
-        }
 
+        }
         
     }
 }
