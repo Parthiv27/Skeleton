2e86b7a3cd67f480d76040fa752d2e9bac64fc61
Ahmed Faraz <p2764411@my365.dmu.ac.uk>
2024-05-19 23:51:00 +0100

    Practical 8 For Supplier Finished without Error

--- AdminSystem/SupplierDataEntry.aspx.cs
+++ AdminSystem/SupplierDataEntry.aspx.cs
@@ -4,10 +4,30 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnOK_Click(object sender, EventArgs e)
+    {
+        clsSupplier ASupplier = new clsSupplier();
+        ASupplier.SupplierID = Convert.ToInt32(txtSupplierID.Text);
+        ASupplier.SupplierName = txtName.Text;
+        ASupplier.Email = txtEmail.Text;
+        ASupplier.Phone = txtPhone.Text;
+        ASupplier.DateReg = Convert.ToDateTime(txtDateReg.Text);
+        ASupplier.Active = Convert.ToBoolean(chkActive.Checked);
+
+        Session["ASupplier"] = ASupplier;
+        Response.Redirect("SupplierViewer.aspx");
+    }
+
+    protected void btnFind_Click(object sender, EventArgs e)
     {
 
     }
--- AdminSystem/SupplierViewer.aspx.cs
+++ AdminSystem/SupplierViewer.aspx.cs
@@ -4,11 +4,19 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        clsSupplier ASupplier = new clsSupplier();
+        ASupplier = (clsSupplier)Session["ASupplier"];
+        Response.Write("Name : " + ASupplier.SupplierName);
+        Response.Write(" Email : " + ASupplier.Email);
+        Response.Write(" Phone : " + ASupplier.Phone);
+        Response.Write(" DateReg : " + ASupplier.DateReg);
+        Response.Write(" Active : " + ASupplier.Active);
 
     }
 }
