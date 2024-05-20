b5750f67b0057cd67fce4db08030efc065856a17
Ahmed Faraz <p2764411@my365.dmu.ac.uk>
2024-05-21 00:49:37 +0100

    Practical 12 Completed without Error

---
+++ ClassLibrary/clsSupplierCollection.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class clsSupplierCollection
+    {
+        List<clsSupplier> mSupplierList = new List<clsSupplier>();
+        clsSupplier mSupplier = new clsSupplier();
+
+        public clsSupplierCollection()
+        {
+            Int32 Index = 0;
+            Int32 RecordCount = 0;
+            clsDataConnection DB = new clsDataConnection();
+            DB.Execute("sproc_tblSupplier_SelectAll");
+            RecordCount = DB.Count;
+
+            while (Index < RecordCount)
+            {
+                clsSupplier ASupplier = new clsSupplier();
+
+                ASupplier.SupplierID = Convert.ToInt32(DB.DataTable.Rows[Index]["SupplierID"]);
+                ASupplier.SupplierName = Convert.ToString(DB.DataTable.Rows[Index]["SupplierName"]);
+                ASupplier.Email = Convert.ToString(DB.DataTable.Rows[Index]["Email"]);
+                ASupplier.Phone = Convert.ToString(DB.DataTable.Rows[Index]["Phone"]);
+                ASupplier.Active = Convert.ToBoolean(DB.DataTable.Rows[Index]["Active"]);
+                ASupplier.DateReg = Convert.ToDateTime(DB.DataTable.Rows[Index]["DateReg"]);
+
+                mSupplierList.Add(ASupplier);
+                Index++;
+            }
+        }
+
+        public List<clsSupplier> SupplierList
+        {
+            get
+            {
+                return mSupplierList;
+            }
+            set
+            {
+                mSupplierList = value;
+            }
+        }
+        public int Count
+        {
+            get
+            {
+                return mSupplierList.Count;
+            }
+            set
+            {
+
+            }
+        }
+        public clsSupplier ThisSupplier
+        {
+            get
+            {
+                return mSupplier;
+            }
+            set
+            {
+                mSupplier = value;
+            }
+        }
+    }
+}
