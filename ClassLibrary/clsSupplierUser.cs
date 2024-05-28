a18240fb1fb91eb184b1d5fa8d97f06f1f4d24d5
Ahmed Faraz <p2764411@my365.dmu.ac.uk>
2024-05-28 16:25:10 +0100

    Practical 15, Login finished

---
+++ ClassLibrary/clsSupplierUser.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class clsSupplierUser
+    {
+        private Int32 mUserID;
+        private String mUsername;
+        private String mPassword;
+        private String mDepartment;
+
+        public int UserID
+        {
+            get
+            {
+                return mUserID;
+            }
+            set
+            {
+                mUserID = value;
+            }
+        }
+
+        public string Username
+        {
+            get
+            {
+                return mUsername;
+            }
+            set
+            {
+                mUsername = value;
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return mPassword;
+            }
+            set
+            {
+                mPassword = value;
+            }
+        }
+
+        public string Department
+        {
+            get
+            {
+                return mDepartment;
+            }
+            set
+            {
+                mDepartment = value;
+            }
+        }
+
+        public bool FindUser(string Username, string Password)
+        {
+            clsDataConnection DB = new clsDataConnection();
+            DB.AddParameter("@Username", Username);
+            DB.AddParameter("@Password", Password);
+            DB.Execute("sproc_tblUsers_FindUsernamePW");
+
+            if (DB.Count == 1)
+            {
+                mUserID = Convert.ToInt32(DB.DataTable.Rows[0]["UserID"]);
+                mUsername = Convert.ToString(DB.DataTable.Rows[0]["Username"]);
+                mPassword = Convert.ToString(DB.DataTable.Rows[0]["Password"]);
+                mDepartment = Convert.ToString(DB.DataTable.Rows[0]["Department"]);
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
